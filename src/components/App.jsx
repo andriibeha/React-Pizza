f0bfed06268833a2530d4f2912c3a66796c565bc
andriibeha <beha.andrii@gmail.com>
2022-09-26 23:09:21 +0300

    Add scss, jsx components

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,28 @@
+import Header from "./Header";
+import Categories from "./Categories";
+import Sort from "./Sort";
+import PizzaBlock from "./PizzaBlock";
+import "../scss/app.scss";
+
 export const App = () => {
   return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
+    <div className="wrapper">
+      <Header />
+      <div className="content">
+        <div className="container">
+          <div className="content__top">
+            <Categories />
+            <Sort />
+          </div>
+          <h2 className="content__title">Все пиццы</h2>
+          <div className="content__items">
+            <PizzaBlock title="Мексиканская" price="500" />
+            <PizzaBlock title="Четыре сыра" price="350" />
+            <PizzaBlock title="Сырная" price="750" />
+            <PizzaBlock title="Веганская" price="550" />
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
