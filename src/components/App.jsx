10c82bcddfb8e2abff14a4a821bbaac7b929242e
andriibeha <beha.andrii@gmail.com>
2022-09-29 20:10:10 +0300

    add React Router Dom

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,39 +1,27 @@
-import { useState } from "react";
+import {Routes,Route} from "react-router-dom";
 import Header from "./Header";
-import Categories from "./Categories";
-import Sort from "./Sort";
-import PizzaBlock from "./PizzaBlock";
+import Home from "pages/Home";
+import NotFound from "pages/NotFound";
+import { Cart } from "pages/Cart";
+
 
 import "../scss/app.scss";
-import { useEffect } from "react";
 
 
 export const App = () => {
-  const [items, setItems] = useState([]);
 
-  useEffect(() => {
-    fetch("https://6304b2f794b8c58fd7231db1.mockapi.io/api/items")
-      .then((res) => res.json())
-      .then((data) => setItems(data))
-  }, []);
 
   return (
     <div className="wrapper">
       <Header />
       <div className="content">
         <div className="container">
-          <div className="content__top">
-            <Categories />
-            <Sort />
-          </div>
-          <h2 className="content__title">Все пиццы</h2>
-          <div className="content__items">
-            {
-              items.map((obj) => (
-                <PizzaBlock key={obj.id} {...obj}/>
-              ))
-            }
-          </div>
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/cart" element={<Cart />} />
+            <Route path="*" element={<NotFound />} />
+
+          </Routes>
         </div>
       </div>
     </div>
---
+++ src/components/NotFoundBlock/index.jsx
@@ -0,0 +1,9 @@
+import s from "./NotFoundBlock.module.scss";
+
+const NotFoundBlock = () => {
+    return (
+        <h1 className={s.root}>404 Not Found</h1>
+    )
+};
+
+export default NotFoundBlock;
